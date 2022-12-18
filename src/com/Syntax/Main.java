e7590cfd3b7d79069da52d024d649c3b51820b95
JuanitaCeci <juanitaceci0624@gmail.com>
2022-12-18 12:25:54 -0500

    The Basic of Solar System

---
+++ src/com/Syntax/Main.java
@@ -0,0 +1,12 @@
+package com.Syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SolarSystem xx= new SolarSystem();
+        xx.moon=1;
+        xx.sun=1;
+        xx.Planet=2;
+    }
+}
