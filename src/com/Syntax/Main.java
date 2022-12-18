c9bb742f8bbecbc904f016c38769fd67869d0794
JuanitaCeci <juanitaceci0624@gmail.com>
2022-12-18 12:40:40 -0500

    Features sun has been added with errors

--- src/com/Syntax/Main.java
+++ src/com/Syntax/Main.java
@@ -8,5 +8,11 @@ public class Main {
         xx.moon=1;
         xx.sun=1;
         xx.Planet=2;
+
+        FeatureSUN Xeron = new FeatureSUN();
+        Xeron.Heat="12324234";
+        Xeron.size=1221334234;
+        Xeron.Temperature="3434";
+
     }
 }
